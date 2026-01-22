d6a1d86b98a9a2c0aa4c767c5f5099a643b90043
vennelakadiri <vennelakadiri2006@gmail.com>
2026-01-22 18:24:02 +0530

    Add Contact component with email information

---
+++ src/components/src/components/src/components/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>Email: johndoe@email.com</p>
+    </section>
+  );
+}
+
+export default Contact;
