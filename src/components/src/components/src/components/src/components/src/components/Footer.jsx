657d753689dec5772982a4f07b3d16e93b98a27d
vennelakadiri <vennelakadiri2006@gmail.com>
2026-01-22 18:24:24 +0530

    Add Footer component with copyright notice

---
+++ src/components/src/components/src/components/src/components/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ padding: "20px", textAlign: "center" }}>
+      © 2026 John Doe
+    </footer>
+  );
+}
+
+export default Footer;
